6a8d1bb9dc6ce219c84f34418f777b05bd3f96d5
Tommy Ng <tommynhm@gmail.com>
2021-01-08 18:24:56 +0800

    First deploy

---
+++ docs/precache-manifest.6695ea0a46115ae9c436644cc60903c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec2873e494b0c2e7fdf",
+    "url": "/css/app.ef551b55.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e28ff873d23fb801c92fc9b3e4e7909d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dec2873e494b0c2e7fdf",
+    "url": "/js/app.75078b8d.js"
+  },
+  {
+    "revision": "914386011bc5793e58bc",
+    "url": "/js/chunk-vendors.0fdcceea.js"
+  },
+  {
+    "revision": "2073b64e28d25ae981c8dd6d0bf10fcd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
