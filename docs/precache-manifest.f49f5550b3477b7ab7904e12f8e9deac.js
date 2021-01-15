108936a8c0a415464f8d2b8cfdd0a874f4181a02
Tommy Ng <tommynhm@gmail.com>
2021-01-16 01:05:04 +0800

    Production build

---
+++ docs/precache-manifest.f49f5550b3477b7ab7904e12f8e9deac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4737c7683bf5226a13",
+    "url": "/css/app.58a53c67.css"
+  },
+  {
+    "revision": "eff0426e38838953d5a74e27d8ecc023",
+    "url": "/img/dog-desktop.eff0426e.jpg"
+  },
+  {
+    "revision": "e6af5d00c8b1516ef6c8b0f2a87f7e3c",
+    "url": "/img/dog-mobile.e6af5d00.jpg"
+  },
+  {
+    "revision": "167cdc4f149ce66c266ca234c053c251",
+    "url": "/img/ipad.167cdc4f.png"
+  },
+  {
+    "revision": "dcb0a396eb1a6bdcba58a76a67b84595",
+    "url": "/img/workvalue-sm.dcb0a396.jpg"
+  },
+  {
+    "revision": "cd83a9ffe43211ac3ffa79cd14aeb064",
+    "url": "/img/workvalue.cd83a9ff.jpg"
+  },
+  {
+    "revision": "c419f80f623262dbd93befa4e4404c34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f4737c7683bf5226a13",
+    "url": "/js/app.8f346ad1.js"
+  },
+  {
+    "revision": "55a92415e9fa3fbeeb24",
+    "url": "/js/chunk-vendors.e107a4d6.js"
+  },
+  {
+    "revision": "2073b64e28d25ae981c8dd6d0bf10fcd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.234cffe2081266130ba59bedb4bf7837.js"
+  "/precache-manifest.f49f5550b3477b7ab7904e12f8e9deac.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "slavewilson.github.io"});
